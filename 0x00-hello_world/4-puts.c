3cea056c119319b7b7632cc725c86be61ec04329
AKing9 <kinganointing10@gmail.com>
2023-02-16 14:32:53 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle,followed by a new line."
+ * main - prints "Programming is like building a multilingual
+ * puzzle,followed by a new line."
  *  Return: Always 0.
  */
 
@@ -9,6 +10,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+        return (0);
 }
