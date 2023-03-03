41eaf863325fa2e3bc90ec1442d79cab8a83f25e
AKing9 <kinganointing10@gmail.com>
2023-03-02 19:04:25 -0800

    expect the best

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -1,6 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
  * main - check the code
  *
@@ -8,8 +5,7 @@
  */
 int main(void)
 {
-char s[] = "ROT13(\"rotate by 13 places\", sometimes hyphenated ROT-13)\n
-is a simple letter substitution cipher.\n";
+char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
 char *p;
 
 p = rot13(s);
