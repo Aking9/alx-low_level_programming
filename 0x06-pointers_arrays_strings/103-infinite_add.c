3b72a39e4f961a6500a05339e71716f52819ccc7
AKing9 <kinganointing10@gmail.com>
2023-03-03 16:28:00 -0800

    infinite addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,13 +10,13 @@ void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
+	char temp;
 
 	while (*(n + i) != '\0')
 	{
 		i++;
 	}
 	i--;
-
 	for (j = 0; j < i; j++, i--)
 	{
 		temp = *(n + j);
@@ -29,12 +29,12 @@ void rev_string(char *n)
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
- * @r: pointer to  buffer
+ * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size-r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
@@ -64,5 +64,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size-r)
 			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_tot
-
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
